fd28b768e95b8cf8f3802db9af0c955c408ae994
Andrew Bowman <andrewtbowman@gmail.com>
2023-04-12 18:31:50 -0400

    feat: us21 admin top 5 customers

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,16 @@
 class Customer < ApplicationRecord
   has_many :invoices
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def self.top_five_customers
+    joins(invoices: :transactions)
+      .where(transactions: { result: 'success' })
+      .group('customers.id')
+      .select('customers.*, COUNT(transactions.id) AS successful_transaction_count')
+      .order('successful_transaction_count DESC')
+      .limit(5)
+  end
 end

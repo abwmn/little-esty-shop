c42a76e269f08e112a9336a2aaa5ac9d56991c8c
Andrew Bowman <andrewtbowman@gmail.com>
2023-04-12 20:19:56 -0400

    test: us3 merchant top 5

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -31,6 +31,37 @@ RSpec.describe 'Merchant Show Dashboard Page', type: :feature do
       click_link("My Invoices")
       expect(current_path).to eq(merchant_invoices_path(merchant1))
     end
+
+    it 'I see the names of the top 5 customers with the largest number of successful transactions with my merchant and their transaction count' do
+      merchant1 = create(:merchant, :with_items)
+      customer1, customer2, customer3, customer4, customer5, customer6 = create_list(:customer, 6)
+    
+      [customer1, customer2, customer3, customer4, customer5].each do |customer|
+        invoice = create(:invoice, customer: customer)
+        item = merchant1.items.sample
+        create(:invoice_item, invoice: invoice, item: item)
+        create_list(:transaction, 5, :successful, invoice: invoice)
+      end
+    
+      invoice = create(:invoice, customer: customer6)
+      item = merchant1.items.sample
+      create(:invoice_item, invoice: invoice, item: item)
+      create_list(:transaction, 2, :successful, invoice: invoice)
+    
+      visit merchant_dashboard_path(merchant1)
+
+      # require 'pry'; binding.pry
+    
+      within("#top-customers") do
+        expect(page).to have_content("#{customer1.full_name}: 5 successful transactions")
+        expect(page).to have_content("#{customer2.full_name}: 5 successful transactions")
+        expect(page).to have_content("#{customer3.full_name}: 5 successful transactions")
+        expect(page).to have_content("#{customer4.full_name}: 5 successful transactions")
+        expect(page).to have_content("#{customer5.full_name}: 5 successful transactions")
+    
+        expect(page).not_to have_content("#{customer6.full_name}")
+      end
+    end
   end
 end
 

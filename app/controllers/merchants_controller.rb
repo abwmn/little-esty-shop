7ee3eba9e065055840a87fde1c6227e97d368c9c
Andrew Bowman <andrewtbowman@gmail.com>
2023-04-12 20:20:09 -0400

    feat: us3 merchant top 5

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,5 +1,6 @@
 class MerchantsController < ApplicationController
   def show
     @merchant = Merchant.find(params[:id])
+    @top_customers = @merchant.top_five_customers
   end
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,15 @@
 class Merchant < ApplicationRecord
   has_many :items
+  has_many :invoices, through: :items
+  has_many :customers, through: :invoices
+
+  def top_five_customers
+    customers
+      .joins(invoices: [:transactions, :invoice_items])
+      .where(transactions: { result: 'success' })
+      .select('customers.*, COUNT(transactions.id) AS successful_transactions_count')
+      .group('customers.id')
+      .order(successful_transactions_count: :desc)
+      .limit(5)  
+  end  
 end
